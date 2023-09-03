24ac73cbb274c425461e3396cf01a46f06fc60f8
Sagar Karmoker <98447327+SagarKarmoker@users.noreply.github.com>
2023-09-03 18:29:43 +0000

    popular model set

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ from flask import Flask, render_template, request
 import numpy as np
 import pandas as pd
 from model import get_recommendations
+from popular_collab import popular_product
 
 app = Flask(__name__)
 
 data = pd.read_csv("./amazon.csv")
 recommend_products = []  # Initialize an empty list to store recommended indices
+popular_products = []
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -30,6 +32,72 @@ def index():
     else:
         random_sample = data.sample(n=10)  # Get a random sample of 10 rows
         return render_template("index.html", user_input=user_input, search_results=search_results, random_sample=random_sample)
+
+
+@app.route("/content-based", methods=["GET", "POST"])
+def content():
+    recommendations = None
+    search_results = None
+    user_input = None  # Initialize user_input here
+
+    if request.method == "POST":
+        user_input = request.form.get("user_input")
+        
+        # Clear the list of recommended products before getting new recommendations
+        recommend_products.clear()
+        
+        recommendations = get_recommendations(user_input)
+        recommend_products.extend(recommendations)  # Add new recommendations to the list
+        
+        recommended_rows = data.loc[recommendations]
+
+        search_results = f"You searched result for: {user_input}"
+        return render_template("index.html", user_input=user_input, random_sample=recommended_rows, search_results=search_results, data=data)
+    else:
+        random_sample = data.sample(n=10)  # Get a random sample of 10 rows
+        return render_template("index.html", user_input=user_input, search_results=search_results, random_sample=random_sample)
+    
+@app.route("/collaborative-filtering", methods=["GET", "POST"])
+def collab():
+    recommendations = None
+    search_results = None
+    user_input = None  # Initialize user_input here
+
+    if request.method == "POST":
+        user_input = request.form.get("user_input")
+        
+        # Clear the list of recommended products before getting new recommendations
+        recommend_products.clear()
+        
+        recommendations = get_recommendations(user_input)
+        recommend_products.extend(recommendations)  # Add new recommendations to the list
+        
+        recommended_rows = data.loc[recommendations]
+
+        search_results = f"You searched result for: {user_input}"
+        return render_template("collab.html", user_input=user_input, random_sample=recommended_rows, search_results=search_results, data=data)
+    else:
+        random_sample = data.sample(n=10)  # Get a random sample of 10 rows
+        return render_template("collab.html", user_input=user_input, search_results=search_results, random_sample=random_sample)
+    
+@app.route("/popularity-based", methods=["GET", "POST"])
+def popular():
+    recommendations = None
+    search_results = None
+    user_input = None
+    result_dict = {}  # Initialize result_dict here
+
+    if request.method == "POST":
+        user_input = request.form.get("user_input")
+        if user_input.isdigit():
+            # Assuming user_input is a valid integer
+            recommendations = popular_product(int(user_input))
+            # Store the recommendations in the global popular_products list
+            popular_products.extend(recommendations)
+            result_dict = recommendations.to_dict(orient='records')
+
+    return render_template("popular.html", search_results=result_dict, user_input=user_input)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ popular_collab.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import numpy as np
+from datetime import datetime
+
+def popular_product(rating: int):
+    products = pd.read_json('/workspaces/CSE366/Dataset_new/products.json')
+    customers = pd.read_json('/workspaces/CSE366/Dataset_new/customers.json')
+    ratings = pd.read_json('/workspaces/CSE366/Dataset_new/ratings.json')
+
+    # Drop the 'CreateDate' column from the ratings DataFrame
+    ratings.drop(columns=["CreateDate"], inplace=True)
+
+    # Rename the 'Id' column in the products DataFrame to 'ProductID'
+    products.rename(columns={'Id': 'ProductID'}, inplace=True)
+
+    # Merge ratings with products on 'ProductID' to get product names
+    ratings_with_name = ratings.merge(products, on='ProductID')
+
+    # Group by product name and count the number of ratings
+    num_rating_df = ratings_with_name.groupby('Name').count()['Rate'].reset_index()
+    num_rating_df.rename(columns={'Rate': 'nums-ratings'}, inplace=True)
+
+    # Group by product name and calculate the average rating
+    avg_rating_df = ratings_with_name.groupby('Name').mean()['Rate'].reset_index()
+    avg_rating_df.rename(columns={'Rate': 'avg-ratings'}, inplace=True)
+
+    # Merge the two dataframes to get 'nums-ratings' and 'avg-ratings'
+    popular_df = num_rating_df.merge(avg_rating_df, on='Name')
+
+    # Filter products with a minimum number of ratings (rating) and sort by 'avg-ratings' in descending order
+    popular_df = popular_df[popular_df['nums-ratings'] >= rating].sort_values('avg-ratings', ascending=False)
+
+    # Take the top 5 products
+    popular_df = popular_df.head(10)
+
+    return popular_df[['Name', 'nums-ratings', 'avg-ratings']]
+
+# # Usage example:
+# popular_products = popular_product(50)  # Example rating threshold
+# print(popular_products)
